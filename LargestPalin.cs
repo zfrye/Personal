33c19bfb2bc99cad196c8a5a2fa700257375e1f3
Zachery Frye <Zac1Frye@gmail.com>
2019-03-07 20:41:09 -0600

    Creation of LargestPalin
    This program will find the largest palindrome from the product of two 7 digit numbers.

---
+++ LargestPalin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Palin
+{
+    class Program
+    {
+        private static long lowerbound = 0L;
+        private static long maxPalin = 0L;
+
+        static void Main(string[] args)
+        {
+            lowerbound = 5000000L;
+
+            Find();
+
+        }
+
+        private static bool IsPalin(long i)
+        {
+            string str = i.ToString();
+            return str.SequenceEqual(str.Reverse());
+        }
+
+        private static void Find()
+        {
+            for (long i = 9999999; i >= (lowerbound); i--)
+            {
+                for (long k = i; k >= 1000000; k--)
+                {
+                    if (i * k > maxPalin && IsPalin(i * k))
+                    {
+                        maxPalin = i * k;
+                        Console.WriteLine("System: " + maxPalin);
+                        Console.WriteLine("Tracker: i = " + i + ", lower = " + lowerbound);
+                        lowerbound = k + 1;
+                    }
+                }
+            }
+            Console.WriteLine("System: Done.");
+        }
+    }
+}
